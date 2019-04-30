20fe4e4f9a9213755d199348269e104c8bad70f7
Thomas Faguet <th.faguet@gmail.com>
2019-04-30 18:28:54 +0200

    test winForm

--- PyRevit/MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Thomas2.pushbutton/script.py
+++ PyRevit/MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Thomas2.pushbutton/script.py
@@ -1,22 +1,264 @@
 # -*- coding: utf-8 -*-
-
+e_a = str("\xe9")
+a_a = str("\xe0")
 # IMPORT
-import clr
 import math
 import System 
 import rpw
+import clr
+clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPIUI') 
 from rpw import revit, db, ui, DB, UI
 from rpw.ui.forms import *
 from System import Array
 from System.Runtime.InteropServices import Marshal
 import sys
 import Autodesk
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI import *
 
 
-e_a = str("\xe9")
-a_a = str("\xe0")
+
+# # COLLECT GENERIC MODEL ///////////////////
+# genericModel_category = db.Collector(of_category = 'OST_GenericModel' , is_type = True)
+# genericModel_element = genericModel_category.get_elements()
+# genericModel_element_CategoryId = [model.Category.Id for model in  genericModel_element]
+# # CREATE SCHEDULE VIEW ///////////////////////////////////////
+# newSchedule = []
+# with db.Transaction('create schedule view'):
+# 	newSchedule.append(DB.ViewSchedule.CreateSchedule(revit.doc, genericModel_element_CategoryId[0]))
+# 	schedulableFields = [sc.Definition.GetSchedulableFields() for sc in newSchedule]
+# # //////////////////////////////////////////////////////
 
 
+# /////////////////////////////////////////////////////////////////////////
+import clr
+
+clr.AddReference('System.Drawing')
+clr.AddReference('System.Windows.Forms')
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+
+class MyListBox(Form):
+	"""docstring for ClassName"""
+	def __init__(self):
+		self.Text = "Ma form cool"
+		screenSize = Screen.GetWorkingArea(self)
+		self.Height = screenSize.Height / 2
+		self.Width = screenSize.Width / 3
+
+		label1 = Label()
+		label1.Parent = self
+		label1.Text = "la liste 1"
+		label1.Location = Point(5,5)
+
+		listbox1 = ListBox()
+		listbox1.Parent = self
+		listbox1.Location = Point(5,30)
+		listbox1.Size = Size(180,100)
+		for k in range(50):
+			listbox1.Items.Add("item" + str(k))
+
+
+
+		label2 = Label()
+		label2.Parent = self
+		label2.Text = "la liste 2"
+		label2.Location = Point(200,5)
+
+		listbox2 = ListBox() 
+		listbox2.Parent = self
+		listbox2.Location = Point(200,30)
+		listbox2.Size = Size(180,100)
+		for k in range(50):
+			listbox2.Items.Add("item" + str(k))
+
+
+
+		label3 = Label()
+		label3.Parent = self
+		label3.Text = "la liste 3"
+		label3.Location = Point(400,5)
+
+		listbox3 = ListBox()
+		listbox3.Parent = self
+		listbox3.Location = Point(400,30)
+		listbox3.Size = Size(180,100)
+		for k in range(50):
+			listbox3.Items.Add("item" + str(k))
+
+
+
+		buttonAdd = Button()
+		buttonAdd.Parent = self
+		buttonAdd.Text = "Ajouter filtre"
+		buttonAdd.Location = Point(450,150)
+		buttonAdd.Size = Size(100,25)
+
+
+		buttonOk = Button()
+		buttonOk.Parent = self
+		buttonOk.Text = "Ok Chef"
+		buttonOk.Location = Point(500,420)
+
+
+		buttonCancel = Button()
+		buttonCancel.Parent = self
+		buttonCancel.Text = "ANNULE"
+		buttonCancel.Location = Point(400,420)
+
+
+		groupBox1 = GroupBox()
+		groupBox1.Parent = self
+		groupBox1.Text = "Détail des filtres"
+		groupBox1.Location = Point(50,200)
+		groupBox1.Size = Size(500,200)
+
+		
+		for k in range(1,9):
+			label1 = Label()
+			label1.Parent = groupBox1
+			label1.Text = " "
+			label1.Location = Point(40,20*k)
+			label1.Size = Size(120,20)
+			label1.BorderStyle = BorderStyle.Fixed3D
+
+			label2 = Label()
+			label2.Parent = groupBox1
+			label2.Text = " "
+			label2.Location = Point(200,20*k)
+			label2.Size = Size(120,20)
+			label2.BorderStyle = BorderStyle.Fixed3D
+			
+			label3 = Label()
+			label3.Parent = groupBox1
+			label3.Text = " "
+			label3.Location = Point(350,20*k)
+			label3.Size = Size(120,20)
+			label3.BorderStyle = BorderStyle.Fixed3D
+
+
+		self.CenterToScreen()
+
+Application.Run(MyListBox())
+
+# ///////////////////////////////////////////////////////
+
+# class HelloWorld3Form(Form):
+#     def __init__(self):
+#         self.Text = "Hello World 3"
+#         self.FormBorderStyle = FormBorderStyle.FixedDialog
+
+#         screenSize = Screen.GetWorkingArea(self)
+#         self.Height = screenSize.Height / 3
+#         self.Width = screenSize.Width / 3
+
+#         self.panelHeight = self.ClientRectangle.Height / 2
+
+#         self.setupPanel1()
+#         self.setupPanel2()
+#         self.setupCounters()
+
+#         self.Controls.Add(self.panel1)
+#         self.Controls.Add(self.panel2)
+
+#     def setupPanel1(self):
+#         self.panel1 = Panel()
+#         self.panel1.BackColor = Color.LightSlateGray
+#         self.panel1.ForeColor = Color.Blue
+#         self.panel1.Width = self.Width
+#         self.panel1.Height = self.panelHeight
+#         self.panel1.Location = Point(0, 0)
+#         self.panel1.BorderStyle = BorderStyle.FixedSingle
+
+#         self.label1 = Label()
+#         self.label1.Text = "Go On - Press Me"
+#         self.label1.Location = Point(20, 20)
+#         self.label1.Height = 25
+#         self.label1.Width = 175
+
+#         self.button1 = Button()
+#         self.button1.Name = '1'
+#         self.button1.Text = 'Press Me 1'
+#         self.button1.Location = Point(20, 50)
+#         self.button1.Click += self.update
+
+#         self.panel1.Controls.Add(self.label1)
+#         self.panel1.Controls.Add(self.button1)
+
+#     def setupPanel2(self):
+#         self.panel2 = Panel()
+#         self.panel2.BackColor = Color.LightSalmon
+#         self.panel2.Width = self.Width
+#         self.panel2.Height = self.panelHeight
+#         self.panel2.Location = Point(0, self.panelHeight)
+#         self.panel2.BorderStyle = BorderStyle.FixedSingle
+
+#         self.subpanel1 = Panel()
+#         self.subpanel1.BackColor = Color.Wheat
+#         self.subpanel1.Width = 175
+#         self.subpanel1.Height = 100
+#         self.subpanel1.Location = Point(25, 25)
+#         self.subpanel1.BorderStyle = BorderStyle.Fixed3D
+
+#         self.label2 = Label()
+#         self.label2.Text = "Go On - Press Me"
+#         self.label2.Location = Point(20, 20)
+#         self.label2.Height = 25
+#         self.label2.Width = 175
+
+#         self.button2 = Button()
+#         self.button2.Name = '2'
+#         self.button2.Text = 'Press Me 2'
+#         self.button2.Location = Point(20, 50)
+#         self.button2.Click += self.update
+
+#         self.subpanel1.Controls.Add(self.label2)
+#         self.subpanel1.Controls.Add(self.button2)
+
+#         self.subpanel2 = Panel()
+#         self.subpanel2.BackColor = Color.Transparent
+#         self.subpanel2.Width = 175
+#         self.subpanel2.Height = 100
+#         self.subpanel2.Location = Point(220, 25)
+#         self.subpanel2.BorderStyle = BorderStyle.Fixed3D
+
+#         self.label3 = Label()
+#         self.label3.Text = "Go On - Press Me"
+#         self.label3.Location = Point(20, 20)
+#         self.label3.Height = 25
+#         self.label3.Width = 175
+
+#         self.button3 = Button()
+#         self.button3.Name = '3'
+#         self.button3.Text = 'Press Me 3'
+#         self.button3.Location = Point(20, 50)
+#         self.button3.Click += self.update
+
+#         self.subpanel2.Controls.Add(self.label3)
+#         self.subpanel2.Controls.Add(self.button3)
+
+#         self.panel2.Controls.Add(self.subpanel1)
+#         self.panel2.Controls.Add(self.subpanel2)
+
+#     def setupCounters(self):
+#         self.counterDict = {
+#             '1': 0,
+#             '2': 0,
+#             '3': 0,
+#         }
+
+#     def update(self, sender, event):
+#         name = sender.Name
+#         self.counterDict[name] += 1
+#         label = getattr(self, 'label' + name)
+#         label.Text = "You have pressed me %s times." % self.counterDict[name]
+
+
+# form = HelloWorld3Form()
+# Application.Run(form)
 
 # # //////////////////////////////////////////////////////////////////////////
 # # COLLECT VIEWS
