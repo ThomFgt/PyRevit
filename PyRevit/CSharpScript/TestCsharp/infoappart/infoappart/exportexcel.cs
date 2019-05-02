b73b20a8c2828e9de9f04f7cd2a7093af25f5f28
krulios <jsable@b-p.fr>
2019-05-02 17:38:00 +0200

    delete useless file

---
+++ PyRevit/CSharpScript/TestCsharp/infoappart/infoappart/exportexcel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using revitapi = Autodesk.Revit.DB;
+using revitui = Autodesk.Revit.UI;
+using revitattri = Autodesk.Revit.Attributes;
+
+namespace infoappart
+{
+    class exportexcel
+    {
+        public string path = @"C:\Users\JulesSablé\Desktop\RECUPINFOPIECE";
+        public Dictionary<string, List<double>> dico1;
+        public Dictionary<string, double> dico2;
+        public List<string> typelog;
+
+        public exportexcel(Dictionary<string, List<double>> dic1, Dictionary<string,double> dic2, List<string> typelogement )
+        {
+            this.dico1 = dic1;
+            this.dico2 = dic2;
+            this.typelog = typelogement;
+            Excel.Application xlapp = new Excel.Application();
+
+            try
+            {
+                if (xlapp == null)
+                {
+                    revitui.TaskDialog.Show("erreur", "excel pas installe");
+                }
+
+                object misvalue = System.Reflection.Missing.Value;
+                xlapp.DisplayAlerts = false;
+                Excel.Workbook workbook = xlapp.Workbooks.Add(misvalue);
+                Excel.Worksheet worksheet1 = workbook.Sheets.Item[1];
+                worksheet1.Name = "Information Appartement";
+                Excel.Worksheet worksheet2 = workbook.Sheets.Add(misvalue, worksheet1, misvalue, misvalue);
+                worksheet2.Name = "Pièces non attribuées";
+                //xlapp.Sheets.add(Type:"template.xlsx");
+                revitui.TaskDialog.Show("exportexcel","app excel ouverte et feuillles creee");
+                int ligne1 = 2;
+                foreach (string key in dico1.Keys)
+                {
+                    worksheet1.Cells[ligne1, 1] = key;
+                    worksheet1.Cells[ligne1, 3] = dico1[key][0];
+                    worksheet1.Cells[ligne1, 4] = dico1[key][1];
+                    worksheet1.Cells[ligne1, 5] = dico1[key][2];
+                    worksheet1.Cells[ligne1, 6] = dico1[key][3];
+                    worksheet1.Cells[ligne1, 7] = dico1[key][4];
+                    ligne1++;
+                }
+                int ligne3 = 2;
+                foreach (string typlog in typelog)
+                {
+                    worksheet1.Cells[ligne3, 2] = typlog;
+                    ligne3++;
+                }
+                int ligne2 = 2;
+                foreach (string key in dico2.Keys)
+                {
+                    worksheet2.Cells[ligne2, 1] = key;
+                    worksheet2.Cells[ligne2, 2] = dico2[key];
+                    ligne2++;
+                }
+                revitui.TaskDialog.Show("exportexcel", "att sauvegarde");
+                workbook.SaveAs(path, Excel.XlFileFormat.xlWorkbookNormal, misvalue, misvalue, misvalue, misvalue, Excel.XlSaveAsAccessMode.xlExclusive, misvalue, misvalue, misvalue, misvalue, misvalue);
+                workbook.Close(true, misvalue, misvalue);
+                xlapp.Quit();
+                Marshal.ReleaseComObject(worksheet1);
+                Marshal.ReleaseComObject(worksheet2);
+                Marshal.ReleaseComObject(workbook);
+                Marshal.ReleaseComObject(xlapp);
+            }
+            catch 
+            {
+                revitui.TaskDialog.Show("erreur", "gros probleme");
+            }
+
+        }
+
+    }
+}
---
+++ PyRevit/CSharpScript/TestCsharp/infoappart/infoappart/recupinfopiece.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System.Windows;
+
+namespace infoappart
+{
+    [TransactionAttribute(TransactionMode.ReadOnly)]
+    public class recupinfopiece : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+            //Dictionnaire : numero appart; liste : nbr piece, shab, surface vitre
+            Dictionary<string, List<double>> DicoAppart = new Dictionary<string, List<double>>();
+            Dictionary<string, double> DicoCommun = new Dictionary<string, double>();
+            //colllecteur de piece
+            FilteredElementCollector doccollecteur = new FilteredElementCollector(doc);
+            IList<Element> listepiece = doccollecteur.OfCategory(BuiltInCategory.OST_Rooms).WhereElementIsNotElementType().ToElements();
+            ElementCategoryFilter filtresol = new ElementCategoryFilter(BuiltInCategory.OST_Floors);
+            ElementCategoryFilter filtreplafond = new ElementCategoryFilter(BuiltInCategory.OST_Ceilings);
+            List<string> typedelogement = new List<string>();
+
+            foreach (Element piece in listepiece)
+            {
+                            
+                double surfacepiece = piece.LookupParameter("Surface").AsDouble();
+                surfacepiece = UnitUtils.ConvertFromInternalUnits(surfacepiece, DisplayUnitType.DUT_SQUARE_METERS);
+                surfacepiece = Math.Round(surfacepiece, 2);
+                string appart = piece.LookupParameter("Appartement").AsString();
+                string cage = piece.LookupParameter("Cage").AsString();
+                string cageappart = cage + appart;
+                double hsfp = 0.00;
+                double hsfs = 0.00;
+                string nompiece = piece.Name.ToString();
+                string typeappart = piece.LookupParameter("Type de logement").AsString();
+
+
+
+
+                // liste de lensemble des donnees de la piece
+                List<double> donneappart = new List<double>();
+
+
+
+
+                // ON CALCULE LA HAUTEUR SOUS PLAFOND OU DALLE
+                try
+                {
+                    LocationPoint locpiece = piece.Location as LocationPoint;
+                    XYZ ptpiece = locpiece.Point;
+                    ptpiece = new XYZ(ptpiece.X, ptpiece.Y, ptpiece.Z + 0.1);
+                    XYZ vecteurpiece = new XYZ(0, 0, 1);
+                    ReferenceIntersector refi = new ReferenceIntersector(filtreplafond, FindReferenceTarget.Face, (View3D)doc.ActiveView);
+                    ReferenceWithContext refc = refi.FindNearest(ptpiece, vecteurpiece);
+                    Reference reference = refc.GetReference();
+                    XYZ intpoint = reference.GlobalPoint;
+                    hsfp = ptpiece.DistanceTo(intpoint);
+                    hsfp = UnitUtils.ConvertFromInternalUnits(hsfp, DisplayUnitType.DUT_METERS);
+                    hsfp = Math.Round(hsfp, 2);
+                    //MessageBox.Show(string.Format("hauteur sous dalle : {0}", hsfp));
+                }
+                catch (Exception)
+                {
+                    //MessageBox.Show("pas de plafond au dessus de la piece","erreur");
+                    hsfp = 0.00;
+
+                }
+
+                // on verifie si sol au dessus de piece
+                try
+                {
+                    LocationPoint locpiece = piece.Location as LocationPoint;
+                    XYZ ptpiece = locpiece.Point;
+                    ptpiece = new XYZ(ptpiece.X, ptpiece.Y, ptpiece.Z + 0.1);
+                    XYZ vecteurpiece = new XYZ(0, 0, 1);
+                    ReferenceIntersector refi = new ReferenceIntersector(filtresol, FindReferenceTarget.Face, (View3D)doc.ActiveView);
+                    ReferenceWithContext refc = refi.FindNearest(ptpiece, vecteurpiece);
+                    Reference reference = refc.GetReference();
+                    XYZ intpoint = reference.GlobalPoint;
+                    hsfs = ptpiece.DistanceTo(intpoint);
+                    hsfs = UnitUtils.ConvertFromInternalUnits(hsfs, DisplayUnitType.DUT_METERS);
+                    hsfs = Math.Round(hsfs, 2);
+                    //MessageBox.Show(string.Format("hauteur sous dalle : {0}", hsfs));
+                }
+                catch (Exception)
+                {
+                    //MessageBox.Show("pas de sol au dessus de la piece","erreur");
+                    hsfs = 0.00;
+                }
+
+                // SI LAPPARTEMENT CONTENANT LA PIECE EST REPERTORIE    
+                if (appart != "" && cage != "" && appart != null && cage != null)
+                {
+
+                    //si l'appartement est repertorie
+                    if (DicoAppart.ContainsKey(cageappart))
+                    {
+                        if (nompiece.Contains("BALCON") | nompiece.Contains("TERRASSE"))
+                        {
+                            DicoAppart[cageappart][4] += surfacepiece;
+                        }
+                        else
+                        {
+                            DicoAppart[cageappart][0] += 1.00;
+                            DicoAppart[cageappart][1] += surfacepiece;
+
+                            //si le faud plafond calcule est plus faible que le precedent
+                            if (hsfp <= DicoAppart[cageappart][2] && hsfp > 1.00)
+                            {
+                                DicoAppart[cageappart][2] = hsfp;
+                            }
+                            if (hsfs <= DicoAppart[cageappart][2] && hsfs > 1.00)
+                            {
+                                DicoAppart[cageappart][2] = hsfs;
+                            }
+                        }
+                    }
+
+                    // SI LAPPARTEMENT CONTENANT LA PIECE NEST PAS REPERTORIE
+                    else
+                    {
+                        if (nompiece.Contains("BALCON") | nompiece.Contains("TERRASSE"))
+                        {
+                            donneappart.Add(0.00);
+                            donneappart.Add(0.00);
+                            donneappart.Add(0.00);
+                            donneappart.Add(0.00);
+                            donneappart.Add(surfacepiece);
+                        }
+                        else
+                        {
+                            donneappart.Add(1.00);
+                            donneappart.Add(surfacepiece);
+
+                            if (hsfp > 1.00)
+                            {
+                                donneappart.Add(hsfp);
+                            }
+                            else
+                            {
+                                donneappart.Add(0.00);
+                            }
+                            if (hsfs > 1.00)
+                            {
+                                donneappart.Add(hsfs);
+                            }
+                            else
+                            {
+                                donneappart.Add(0.00);
+                            }
+                            donneappart.Add(0.00);
+                            typedelogement.Add(typeappart);
+                        }
+                        DicoAppart.Add(cageappart, donneappart);
+
+                    }
+
+                }
+                else
+                {
+                    if (DicoCommun.ContainsKey(nompiece))
+                    {
+                        DicoCommun[nompiece] += surfacepiece;
+                    }
+                    else
+                    {
+                        DicoCommun.Add(nompiece, surfacepiece);
+                    }
+                }
+
+            }
+            try
+            {
+                exportexcel export = new exportexcel(DicoAppart,DicoCommun,typedelogement);
+                return Result.Succeeded;
+            }
+            catch
+            {
+                TaskDialog.Show("dommage", "pas d'excel enregistre");
+                return Result.Failed;
+            }
+
+            // MESSAGE BOX DES APPARTEMENTS
+            //foreach (string a in DicoAppart.Keys)
+            //{
+            //    _ = MessageBox.Show(string.Format("Nombre de pièce : {0}" + Environment.NewLine +
+            //    "surface habitable : {1}" + Environment.NewLine +
+            //    "hauteur sous plafond {2}" + Environment.NewLine +
+            //    "Hauteur sous dalle {3}" + Environment.NewLine +
+            //    "Surface du balcon {4}",
+            //    DicoAppart[a][0],
+            //    DicoAppart[a][1],
+            //    DicoAppart[a][2],
+            //    DicoAppart[a][3],
+            //    DicoAppart[a][4]),
+            //    string.Format("numero d'appartement : {0}", a));
+
+            //}
+
+
+        }
+    }
+}
