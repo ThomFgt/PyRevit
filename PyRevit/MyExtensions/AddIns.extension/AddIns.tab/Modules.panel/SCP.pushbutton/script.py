d9df3b41062ff44c7029d7eb6ffd86ae7d62f2fc
Thomas Faguet <th.faguet@gmail.com>
2019-08-12 10:53:35 +0200

    many new stuff

--- PyRevit/MyExtensions/AddIns.extension/AddIns.tab/Modules.panel/SCP.pushbutton/script.py
+++ PyRevit/MyExtensions/AddIns.extension/AddIns.tab/Modules.panel/SCP.pushbutton/script.py
@@ -13,9 +13,261 @@ from Autodesk.Revit.UI import *
 from System.Collections.Generic import List
 from pyrevit import forms
 from rpw.ui.forms import TextInput
+from pyrevit import framework
+from pyrevit.framework import Interop
+from pyrevit.api import AdWindows
+from pyrevit.framework import wpf, Forms, Controls, Media
 
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
+
+class WPFWindow(framework.Windows.Window):
+    def __init__(self, xaml_source, literal_string=False):
+        # self.Parent = self
+        wih = Interop.WindowInteropHelper(self)
+        wih.Owner = AdWindows.ComponentManager.ApplicationWindow
+
+        if not literal_string:
+            if not op.exists(xaml_source):
+                wpf.LoadComponent(self,
+                                  os.path.join(EXEC_PARAMS.command_path,
+                                               xaml_source)
+                                  )
+            else:
+                wpf.LoadComponent(self, xaml_source)
+        else:
+            wpf.LoadComponent(self, framework.StringReader(xaml_source))
+
+        #2c3e50
+        self.Resources['pyRevitDarkColor'] = \
+            Media.Color.FromArgb(0xFF, 0x2c, 0x3e, 0x50)
+
+        #23303d
+        self.Resources['pyRevitDarkerDarkColor'] = \
+            Media.Color.FromArgb(0xFF, 0x23, 0x30, 0x3d)
+
+        #ffffff
+        self.Resources['pyRevitButtonColor'] = \
+            Media.Color.FromArgb(0xFF, 0xff, 0xff, 0xff)
+
+        #f39c12
+        self.Resources['pyRevitAccentColor'] = \
+            Media.Color.FromArgb(0xFF, 0xf3, 0x9c, 0x12)
+
+        self.Resources['pyRevitDarkBrush'] = \
+            Media.SolidColorBrush(self.Resources['pyRevitDarkColor'])
+        self.Resources['pyRevitAccentBrush'] = \
+            Media.SolidColorBrush(self.Resources['pyRevitAccentColor'])
+
+        self.Resources['pyRevitDarkerDarkBrush'] = \
+            Media.SolidColorBrush(self.Resources['pyRevitDarkerDarkColor'])
+
+        self.Resources['pyRevitButtonForgroundBrush'] = \
+            Media.SolidColorBrush(self.Resources['pyRevitButtonColor'])
+
+    def show(self, modal=False):
+        if modal:
+            return self.ShowDialog()
+        self.Show()
+
+    def show_dialog(self):
+        return self.ShowDialog()
+
+    def set_image_source(self, element_name, image_file):
+        wpfel = getattr(self, element_name)
+        if not op.exists(image_file):
+            # noinspection PyUnresolvedReferences
+            wpfel.Source = \
+                framework.Imaging.BitmapImage(
+                    framework.Uri(os.path.join(EXEC_PARAMS.command_path,
+                                               image_file))
+                    )
+        else:
+            wpfel.Source = \
+                framework.Imaging.BitmapImage(framework.Uri(image_file))
+
+    @staticmethod
+    def hide_element(*wpf_elements):
+        for wpfel in wpf_elements:
+            wpfel.Visibility = framework.Windows.Visibility.Collapsed
+
+    @staticmethod
+    def show_element(*wpf_elements):
+        for wpfel in wpf_elements:
+            wpfel.Visibility = framework.Windows.Visibility.Visible
+
+    @staticmethod
+    def toggle_element(*wpf_elements):
+        for wpfel in wpf_elements:
+            if wpfel.Visibility == framework.Windows.Visibility.Visible:
+                self.hide_element(wpfel)
+            elif wpfel.Visibility == framework.Windows.Visibility.Collapsed:
+                self.show_element(wpfel)
+
+class TemplateUserInputWindow(WPFWindow):
+    layout = """
+    <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+            ShowInTaskbar="False" ResizeMode="NoResize"
+            WindowStartupLocation="CenterScreen"
+            HorizontalContentAlignment="Center">
+    </Window>
+    """
+    def __init__(self, context, title, width, height, **kwargs):
+        WPFWindow.__init__(self, self.layout, literal_string=True)
+        self.Title = title
+        self.Width = width
+        self.Height = height
+
+        self._context = context
+        self.response = None
+        self.PreviewKeyDown += self.handle_input_key
+
+        self._setup(**kwargs)
+
+    def _setup(self, **kwargs):
+        pass
+
+    def handle_input_key(self, sender, args):
+        if args.Key == framework.Windows.Input.Key.Escape:
+            self.Close()
+
+    @classmethod
+    def show(cls, context,
+             title='User Input', width=300, height=400, **kwargs):
+        dlg = cls(context, title, width, height, **kwargs)
+        dlg.ShowDialog()
+        return dlg.response
+
+class SelectFromCheckBoxes(TemplateUserInputWindow):
+    layout = """
+    <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+            ShowInTaskbar="False" ResizeMode="NoResize"
+            WindowStartupLocation="CenterScreen"
+            HorizontalContentAlignment="Center">
+            <Window.Resources>
+                <Style x:Key="ClearButton" TargetType="Button">
+                    <Setter Property="Background" Value="White"/>
+                </Style>
+            </Window.Resources>
+            <DockPanel Margin="10">
+                <DockPanel DockPanel.Dock="Top" Margin="0,0,0,10">
+                    <TextBlock FontSize="14" Margin="0,3,10,0" \
+                               DockPanel.Dock="Left">Filter:</TextBlock>
+                    <StackPanel>
+                        <TextBox x:Name="search_tb" Height="25px"
+                                 TextChanged="search_txt_changed"/>
+                        <Button Style="{StaticResource ClearButton}"
+                                HorizontalAlignment="Right"
+                                x:Name="clrsearch_b" Content="x"
+                                Margin="0,-25,5,0" Padding="0,-4,0,0"
+                                Click="clear_search"
+                                Width="15px" Height="15px"/>
+                    </StackPanel>
+                </DockPanel>
+                <StackPanel DockPanel.Dock="Bottom">
+                    <Grid>
+                        <Grid.RowDefinitions>
+                            <RowDefinition Height="Auto" />
+                        </Grid.RowDefinitions>
+                        <Grid.ColumnDefinitions>
+                            <ColumnDefinition Width="*" />
+                            <ColumnDefinition Width="*" />
+                            <ColumnDefinition Width="*" />
+                        </Grid.ColumnDefinitions>
+                        <Button x:Name="checkall_b"
+                                Grid.Column="0" Grid.Row="0"
+                                Content="Check" Click="check_all"
+                                Margin="0,10,3,0"/>
+                        <Button x:Name="uncheckall_b"
+                                Grid.Column="1" Grid.Row="0"
+                                Content="Uncheck" Click="uncheck_all"
+                                Margin="3,10,3,0"/>
+                        <Button x:Name="toggleall_b"
+                                Grid.Column="2" Grid.Row="0"
+                                Content="Toggle" Click="toggle_all"
+                                Margin="3,10,0,0"/>
+                    </Grid>
+                    <Button x:Name="select_b" Content=""
+                            Click="button_select" Margin="0,10,0,0"/>
+                </StackPanel>
+                <ListView x:Name="list_lb">
+                    <ListView.ItemTemplate>
+                         <DataTemplate>
+                           <StackPanel>
+                             <CheckBox Content="{Binding name}"
+                                       IsChecked="{Binding state}"/>
+                           </StackPanel>
+                         </DataTemplate>
+                   </ListView.ItemTemplate>
+                </ListView>
+            </DockPanel>
+    </Window>
+    """
+    def _setup(self, **kwargs):
+        self.hide_element(self.clrsearch_b)
+        self.clear_search(None, None)
+        self.search_tb.Focus()
+
+        button_name = kwargs.get('button_name', None)
+        if button_name:
+            self.select_b.Content = button_name
+
+        self._list_options()
+
+    def _list_options(self, checkbox_filter=None):
+        if checkbox_filter:
+            self.checkall_b.Content = 'Check'
+            self.uncheckall_b.Content = 'Uncheck'
+            self.toggleall_b.Content = 'Toggle'
+            checkbox_filter = checkbox_filter.lower()
+            self.list_lb.ItemsSource = \
+                [checkbox for checkbox in self._context
+                 if checkbox_filter in checkbox.name.lower()]
+        else:
+            self.checkall_b.Content = 'Check All'
+            self.uncheckall_b.Content = 'Uncheck All'
+            self.toggleall_b.Content = 'Toggle All'
+            self.list_lb.ItemsSource = self._context
+
+    def _set_states(self, state=True, flip=False):
+        current_list = self.list_lb.ItemsSource
+        for checkbox in current_list:
+            if flip:
+                checkbox.state = not checkbox.state
+            else:
+                checkbox.state = state
+
+        # push list view to redraw
+        self.list_lb.ItemsSource = None
+        self.list_lb.ItemsSource = current_list
+
+    def toggle_all(self, sender, args):
+        self._set_states(flip=True)
+
+    def check_all(self, sender, args):
+        self._set_states(state=True)
+
+    def uncheck_all(self, sender, args):
+        self._set_states(state=False)
+
+    def button_select(self, sender, args):
+        self.response = self._context
+        self.Close()
+
+    def search_txt_changed(self, sender, args):
+        if self.search_tb.Text == '':
+            self.hide_element(self.clrsearch_b)
+        else:
+            self.show_element(self.clrsearch_b)
+
+        self._list_options(checkbox_filter=self.search_tb.Text)
+
+    def clear_search(self, sender, args):
+        self.search_tb.Text = ' '
+        self.search_tb.Clear()
+        self.list_lb.ItemsSource = self._context
 
 class CheckBoxOption:
 	def __init__(self, name, default_state=False):
@@ -31,7 +283,6 @@ class CheckBoxOption:
 view_list = ["La vue active", "Tout le document"]
 res1 = forms.SelectFromList.show(view_list,
 									multiselect = False,
-									name_attr = "Vue",
 									button_name = "OK")
 
 if res1 != None:
@@ -53,7 +304,7 @@ if res1 != None:
 	bic_list = bic_dir.keys()
 	options = [CheckBoxOption(j) for j in bic_list]
 
-	all_checkboxes = forms.SelectFromCheckBoxes.show(options)
+	all_checkboxes = SelectFromCheckBoxes.show(options)
 
 	res2 = []
 	for checkbox in all_checkboxes:
@@ -96,7 +347,6 @@ if res1 != None:
 
 	res3 = forms.SelectFromList.show(param_dir.keys(),
 										multiselect = False,
-										name_attr = "Parametre",
 										button_name = "OK")
 
 	if res3 != None:
--- PyRevit/MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Thomas2.pushbutton/script.py
+++ PyRevit/MyExtensions/AddIns.extension/AddIns.tab/tests.panel/Thomas2.pushbutton/script.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+__title__ = 'Révisions'
+
+__doc__ = 'Ce programme ajoute les révisions disponibles sur les feuilles sélectionnées'
 
 # IMPORT
 # import math
@@ -22,5 +25,202 @@ clr.AddReference('System.Drawing')
 clr.AddReference('System.Windows.Forms')
 from System.Drawing import *
 from System.Windows.Forms import *
+
+
+# COLLECT VIEWS MAQUETTE
+view_category = DB.FilteredElementCollector(revit.doc).OfCategory(BuiltInCategory.OST_Sheets)
+view_element = view_category.WhereElementIsNotElementType()
+view_name = [views.ViewName for views in view_element]
+view_id = [views.Id for views in view_element]
+
+# COLLECT REVISION MAQUETTE
+revisions_category = DB.FilteredElementCollector(revit.doc).OfCategory(BuiltInCategory.OST_Revisions)
+revision_element = revisions_category.WhereElementIsNotElementType()
+revision_name = [revisions.Name for revisions in revision_element]
+revision_id = [revisions.Id for revisions in revision_element]
+revision_description = [revisions.Description for revisions in revision_element]
+revision_date = [revisions.RevisionDate for revisions in revision_element]
+
+
+sheets_out = []
+sheets_id_out = []
+index = {}
+sheets_id_name = {}
+revision_id_name = {}
+
+for k in range(len(view_name)):
+	sheets_id_name[str(view_name[k])] = view_id[k]
+
+for k in range(len(revision_name)):
+	revision_id_name[str(revision_name[k])] = revision_id[k]
+
+
+# /////////////////////////////////////////////////////////////////////////
+# START WINDOWS FORM
+class MyListBox(Form):
+	# docstring for ClassName
+	def __init__(self):
+		self.Text = "Plugin revision on sheets"
+		self.Size = Size(600,600)
+		self.CenterToScreen()
+		self.FormBorderStyle = FormBorderStyle.FixedToolWindow
+		
+
+	# label for listbox 1 on the left
+		label1 = Label()
+		label1.Parent = self
+		label1.Text = "Double clic sur les feuilles"
+		label1.Location = Point(5,5)
+		label1.Size = Size(180,15)
+	# listbox 1 on the left
+		self.listbox1 = ListBox()
+		self.listbox1.Parent = self
+		self.listbox1.Location = Point(5,30)
+		self.listbox1.Size = Size(280,500)
+		for k in view_name:
+			self.listbox1.Items.Add(k)
+		self.listbox1.DoubleClick += self.OnDoubleClick
+
+
+	# label for listbox 2 on the right
+		label2 = Label()
+		label2.Parent = self
+		label2.Size = Size(180,15)
+		label2.Text = "Liste des feuilles sélectionnées"
+		label2.Location = Point(300,5)
+	# listbox 2 on the right
+		self.listbox2 = ListBox()
+		self.listbox2.Parent = self
+		self.listbox2.Location = Point(300,30)
+		self.listbox2.Size = Size(280,300)
+		for k in sheets_out:
+			self.listbox2.Items.Add(k)
+		self.listbox2.DoubleClick += self.OnDoubleClick2
+
+
+
+	# button OK
+		self.buttonOk = Button()
+		self.buttonOk.Parent = self
+		self.buttonOk.Text = "Ok Chef"
+		self.buttonOk.Location = Point(500,520)
+		self.buttonOk.Click += self.Ok
+	# button Cancel
+		self.buttonCancel = Button()
+		self.buttonCancel.Parent = self
+		self.buttonCancel.Text = "ANNULE"
+		self.buttonCancel.Location = Point(400,520)
+		self.buttonCancel.Click += self.Cancel
+
+
+
+
+	# EVENT ON CLICK //////
+	# Event on double click on listbox1
+	def OnDoubleClick(self,sender,event):
+		item = sender.SelectedItem
+		index[str(item)] = sender.SelectedIndex
+		self.listbox2.Items.Add(item)
+		self.listbox1.Items.Remove(item)
+
+
+	# Event on double click on listbox2
+	def OnDoubleClick2(self,sender,event):
+		item = sender.SelectedItem
+		self.listbox1.Items.Insert(index.get(item), item)
+		self.listbox2.Items.Remove(item)
+		
+
+	# Event on Cancel Button Click
+	def Cancel(self,sender,event):
+		self.Close()
+
+	# Event on OK Button Click
+	def Ok(self,sender,event):
+		for i in range(self.listbox2.Items.Count):
+			sheets_out.append(self.listbox2.Items[i])
+		self.Close()
+
+# run form
+Application.Run(MyListBox())
+# END OF WINDOWS FORM ///////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////
+
+
+revision_out = []
+# /////////////////////////////////////////////////////////////////////////
+# START WINDOWS FORM
+class MyListBox2(Form):
+	# docstring for ClassName
+	def __init__(self):
+		self.Text = "Plugin revision on sheets"
+		self.Size = Size(600,600)
+		self.CenterToScreen()
+		self.FormBorderStyle = FormBorderStyle.FixedToolWindow
+		
+
+	# label for listbox 1 on the left
+		label1 = Label()
+		label1.Parent = self
+		label1.Text = "Choisir les révisions à faire apparaitre sur les feuilles sélectionnées précédemment"
+		label1.Location = Point(5,5)
+		label1.Size = Size(500,30)
+	# listbox 1 on the left
+		self.checkedlistbox1 = CheckedListBox()
+		self.checkedlistbox1.Parent = self
+		self.checkedlistbox1.Location = Point(30,40)
+		self.checkedlistbox1.Size = Size(400,100)
+		for k in revision_name:
+			self.checkedlistbox1.Items.Add(k)
+
+
+	# button OK
+		self.buttonOk = Button()
+		self.buttonOk.Parent = self
+		self.buttonOk.Text = "Ok Chef"
+		self.buttonOk.Location = Point(500,520)
+		self.buttonOk.Click += self.Ok
+	# button Cancel
+		self.buttonCancel = Button()
+		self.buttonCancel.Parent = self
+		self.buttonCancel.Text = "ANNULE"
+		self.buttonCancel.Location = Point(400,520)
+		self.buttonCancel.Click += self.Cancel
+
+
+
+	# EVENT ON CLICK //////
+	# Event on Cancel Button Click
+	def Cancel(self,sender,event):
+		self.Close()
+
+	# Event on OK Button Click
+	def Ok(self,sender,event):
+		for i in range(self.checkedlistbox1.CheckedItems.Count):
+			revision_out.append(self.checkedlistbox1.CheckedItems[i])
+		self.Close()
+
+# run form
+Application.Run(MyListBox2())
+# END OF WINDOWS FORM ///////////////////////////////////////////////////
+# ///////////////////////////////////////////////////////////////////////
+
+
+# Get sheet element selected by user
+sheet_out_elem = []
+for item in sheets_out:
+	sheet_out_elem.append(DB.Document.GetElement(revit.doc, sheets_id_name[item]))
+
+# Get collection element id of revision selected by user
+revision_out_id = []
+for item in revision_out:
+	revision_out_id.append(revision_id_name[item])
+collection = List[DB.ElementId](revision_out_id)
+
+# Transaction
+with db.Transaction("set new revision"):
+	for sheets in sheet_out_elem:
+		sheets.SetAdditionalRevisionIds(collection)
+
 
 
